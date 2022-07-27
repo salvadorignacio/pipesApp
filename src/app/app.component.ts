6a52c224431fa5ffc2181ea1f237a0f5b6d74b54
Salvador Peña <salvadorignacio23@gmail.com>
2022-07-27 17:25:42 -0400

    Se agregan pipes no comunes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'pipesApp';
+
+  constructor(private primeNGConfig : PrimeNGConfig) {
+
+  }
+
+  ngOnInit(){
+    this.primeNGConfig.ripple = true; //Efecto borbuja al hacer click en los elemtos de PrimeNG
+  }
 }
--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -8,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoComunesComponent implements OnInit {
 
+  nombre: string = 'Daniela';
+  genero: string = 'f';
+
+  bienvenidoMapa = {
+    'm': 'bienvenido',
+    'f': 'bienvenida'
+  }
+
+  clientes: string [] = ['Salvador', 'Daniela'];
+
+  clientesMapa = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    'other': 'tenemos # clientes esperando.'
+  }
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  cambiarNombre () {
+    if(this.nombre == 'Daniela') {
+      this.nombre = 'Salvador';
+      this.genero = 'm';
+    } else {
+      this.nombre = 'Daniela';
+      this.genero = 'f';
+    }
+
+  }
+
+  agregarCliente () {
+    this.clientes.push('otro');
+  }
+
+  eliminarCliente () {
+    this.clientes.shift();
+  }
 }
